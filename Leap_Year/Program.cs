f7a31d2693cf8004f63bf93e17500d247b6ea835
Pavan699 <pavannakate03@gmail.com>
2021-09-02 10:09:25 -0700

    Leap_Year_Solution

---
+++ Leap_Year/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Leap_Year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.Write("Enter the Year in YYYY format : ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if((year % 4) == 0)
+            {
+                Console.WriteLine("Year {0} is a Leap-Year.", year);
+            }
+            else
+            {
+                Console.WriteLine("Year {0} is Not a Leap-Year.", year);
+            }
+        }
+    }
+}
