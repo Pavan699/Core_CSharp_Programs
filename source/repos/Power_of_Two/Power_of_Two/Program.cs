abbfc4217f8aeb575d833a497022d9b14c3ff0c2
maha <pavannakate03@gmail.com>
2021-09-02 10:14:20 -0700

    Power of number to the base Two

---
+++ source/repos/Power_of_Two/Power_of_Two/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Power_of_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num, result = 0;
+            Console.Write("Enter The Number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1;i<=num;i++)
+            {
+                result = (Math.Pow(2,i));
+            }
+            Console.WriteLine("The Power of {0} to the base 2 is : {1}", num, result);
+        }
+    }
+}
