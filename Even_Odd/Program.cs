268081131f012cbdca8222b64a219f321e04a248
Pavan699 <pavannakate03@gmail.com>
2021-09-02 11:01:15 -0700

    Pushing EvenOdd Check Code

---
+++ Even_Odd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Even_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to Check Even and Odd Number");
+            int Num;
+            Console.Write("Enter The Number : ");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            if (Num%2 == 0)
+            {
+                Console.WriteLine("The Number {0} is Even.", Num);
+            }
+            else
+            {
+                Console.WriteLine("The Number {0} is Odd.", Num);
+            }
+        }
+    }
+}
