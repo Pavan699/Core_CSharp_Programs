9d390dcf64230c2086a9494db013b4faad465a63
Pavan699 <pavannakate03@gmail.com>
2021-09-02 11:04:23 -0700

    Alphabet is Vowel or Consonant

---
+++ Vowel_Consonant/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Vowel_Consonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vowel and Consonants in a String");
+            string str;
+            Console.Write("Enter The String to Check vowels and consonants :");
+            str = Console.ReadLine();
+            
+            for (int i = 0; i <= str.Length-1; i++ )
+            {
+                if (str[i] == 'a'||str[i] == 'e'||str[i] == 'i'||str[i] == 'o'||str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                {
+                    Console.WriteLine(str[i] + " is a vowel");
+                }
+                else
+                {
+                    Console.WriteLine(str[i] + " is a consonant");
+                }
+            }
+        }
+    }
+}
